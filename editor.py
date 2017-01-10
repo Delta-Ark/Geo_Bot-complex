26ac26d685f52fec66c18148ea097ab049e1fc57
Trevor <trevor.owens@gmail.com>
2017-01-09 17:47:36 -0800

    made editor a little more clear and fixed a bug

--- editor.py
+++ editor.py
@@ -6,19 +6,19 @@
 """Creates a simple text editor
 """
 import curses
-from curses.textpad import Textbox, rectangle
-import traceback
+from curses.textpad import Textbox
 
 
 def emacs_textbox(stdscr, initial_text):
-    commands = """
-    A command Control-G is == Control + g (don't capitalize)
-    To Exit hit Control-G
+    instructions = """
+    To Save and Exit hit Control-G
 
     This editing buffer uses Emacs commands (No Control-Y though)
-
+    *** A command Control-G is == Control + g (don't capitalize) ***
+    ---------------------------------------------------------------
     Movement:
     Use arrow keys
+
     OR:
     Start of line: Control-A
     End of line:   Control-E
@@ -26,41 +26,30 @@ def emacs_textbox(stdscr, initial_text):
     Forward        Control-F
     Down line      Control-N Cursor down; move down one line.
     Previous line  Control-P Cursor up; move up one line.
-    ---------------------------------------------------------------
+
+    COPY + PASTE: Use mouse + keyboard shortcuts to copy and paste
+
     Deletion:
         Delete under cursor  Control-D
         Delete backwards     Control-H
         Kill line            Control-K
     """
-    stdscr.addstr(commands)
-    n_lines = len(commands.splitlines()) - 4 + 1
-    stdscr.addstr("-----------------------------------------\n")
-    stdscr.addstr(initial_text)
-    # lines = 10  #curses.LINES
-    # cols = 80  #curses.COLS
-    # uly = 5
-    # ulx = 1
-    # editwin = curses.newwin(lines, cols, uly, ulx)
-    # rectangle(stdscr, uly, ulx, 1+lines+1, 1+cols+1)
+    stdscr.addstr(instructions)
 
+    ending = """------------------------------------------------------\n
+                     EDIT BELOW ONLY
+    ------------------------------------------------------\n"""
+    stdscr.addstr(ending)
+    stdscr.addstr(initial_text)
     box = Textbox(stdscr)  # from curses.textpad
     box.edit()
     message = box.gather()
-    lines = message.splitlines()
-    message = "\n".join(lines[n_lines:])
-    return message
-
-
-def callable(stdscr):
-    stdscr.clear()
-    stdscr.addstr("Press ESC to exit editor\n")
-    stdscr.addstr(str(stdscr.getmaxyx()))
-    stdscr.addstr("\nsome text \n to edit \n is right here")
-    while True:
-        key = stdscr.getch()
-        if key == 27:
-            break
-    return 0
+    # n_lines = len(instructions.splitlines()) - 4 + 1
+    # lines = message.splitlines()
+    # print lines[n_lines:]
+    # message = "\n".join(lines[n_lines:])
+    remove_index = len(ending) + len(instructions)
+    return message[remove_index + 15:]
 
 
 def create_editor(initial_text):
