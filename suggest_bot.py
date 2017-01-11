6f8ef8d591a5293f16d5e82f294fbaf15e51c1d1
Trevor <trevor.owens@gmail.com>
2017-01-10 19:10:13 -0800

    fixed bug with default words

--- suggest_bot.py
+++ suggest_bot.py
@@ -181,11 +181,6 @@ def main():
         print 'Using parameters from ' + str(args.params)
         # turn parameter file into dictionary
         g.set_params_from_file(args.params)
-
-    if args.input:
-        with codecs.open(args.input, encoding='utf-8', mode='rU') as f:
-            input = f.read()
-        for_poem = utils.tokenize_and_filter(input)
         
     if args.address:
         print "Finding geocoordates for address:\n{}".format(args.address)
@@ -200,7 +195,14 @@ def main():
         else:
             print "Failed to find coordinates. Exiting."
             Sys.exit()
-
+            
+    if args.input:
+        with codecs.open(args.input, encoding='utf-8', mode='rU') as f:
+            input = f.read()
+            for_poem = utils.tokenize_and_filter(input)
+    else:
+        for_poem = get_default_words()
+            
     formatted_poem = create_poem(g, for_poem)
     if args.output:
         print '\nwriting formatted poem to ' + str(args.output)
@@ -217,108 +219,108 @@ def main():
         f.write(formatted_poem)
 
 
-# These are some good default words used in the poem creator above
-for_poem = [
-    # emerging tech shit
-    'Agricultural', 'ecological', 'systems', 'meat', 'genetically',
-    'modified', 'precision', 'vertical', 'farming', 'printing', 'contour',
-    'crafting', 'artificial', 'uterus', 'transplant', 'cryonics',
-    'vitrification', 'suspended animation', 'de-extinction',
-    'genetic engineering', 'gene therapy', 'life extension',
-    'engineered negligible senescence',
-    'nanomedicine', 'nanosensors', 'regenerative', 'medicine',
-    'stem-cell', 'tissue engineering', 'robot assisted surgery',
-    'synthetic biology', 'synthetic genomics', 'virus',
-    'whole genome sequencing', 'bionic contact lens',
-    'head-mounted display', 'virtual',
-    'retinal', 'e-textiles', 'molecular', 'electronics', 'thermal',
-    'copper', 'pillar', 'airborne wind turbine', 'artificial',
-    'photosynthesis', 'biofuels', 'solar', 'power', 'fusion', 'fuel cell',
-    'molten salt', 'photovoltaic', 'translation', 'machine vision',
-    'speech recognition', 'fourth-generation', 'optical discs', 'storage',
-    'holographic data', 'millipede', 'optical computing',
-    'quantum computing', 'quantum cryptography', 'RFID', 'software-defined',
-    'three-dimensional', 'integrated', 'circuit', 'artificial muscle',
-    'superconductivity', 'superfluidity', 'metamaterials', 'cloaking',
-    'metal', 'multi-function', 'superalloy', 'synthetic diamond',
-    'weapon', 'laser', 'particle-beam', 'coilgun', 'plasma', 'stealth',
-    'brain computer interface', 'retinal implant',
-    'self reconfiguring modular robot', 'swarm robotics', 'pulse',
-    'solar sail', 'backpack',
-    'helicopter', 'delivery drone', 'detonation', 'engine', 'driverless\
-    car', 'automated', 'vacuum', 'collection', 'cloak', 'immersive',
-    'dilemma',
-    # japanese shit
-    'august', 'black', 'chinese', 'Gaugin', 'heaven', 'illusion',
-    'island', 'Kibune', 'Michinoku', 'milky', 'Mogami', 'mother',
-    'mount', 'mountain', 'Musashi', 'night', 'observe', 'October',
-    'portrait', 'river', 'Roman', 'SUNSHINE', 'should', 'submit',
-    'tangled', 'Tokiwa', 'washing', 'watching', 'world', 'Yoshino',
-    'actual', 'admires', 'after', 'afterlife', 'again', 'against',
-    'alive', 'almost', 'always', 'amidah', 'ancient', 'another',
-    'armor', 'armored', 'arrayed', 'arrows', 'autumn', 'autumns',
-    'awakening', 'bamboo', 'bathe', 'beads', 'become', 'becoming',
-    'begins', 'behind', 'between', 'beyond', 'birth', 'blade',
-    'blind', 'bloom', 'blooming', 'blossoms', 'break', 'breaks',
-    'breeze', 'bridge', 'brings', 'brother', 'brush', 'buried',
-    'burning', 'butterfly', 'calligraphy', 'calling', 'camellia',
-    'cancer', 'candle', 'canyon', 'caress', 'carry', 'ceaseless',
-    'cedars', 'center', 'certain', 'change', 'chanted', 'chases',
-    'cherries', 'cherry', 'child', 'chill', 'chorus', 'chrysanthemum',
-    'chrysanthemums', 'cicada', 'clock', 'closer', 'color', 'combing',
-    'compare', 'completely', 'content', 'continent', 'corona',
-    'could', 'crest', 'crossing', 'curve', 'dancers', 'darkens',
-    'darkness', 'death', 'deepens', 'delusions', 'deserted',
-    'destitute', 'distance', 'dream', 'dreaming', 'dreams', 'drips',
-    'drops', 'drums', 'dying', 'early', 'eclipse', 'egret', 'ended',
-    'entangling', 'escaped', 'evening', 'every', 'exhausted',
-    'faintly', 'falling', 'falls', 'feeling', 'field', 'finished',
-    'fireflies', 'firefly', 'fireworks', 'first', 'flash', 'flesh',
-    'flies', 'float', 'flowers', 'flowing', 'flows', 'follow',
-    'forever', 'forlorn', 'forth', 'fragile', 'frozen', 'garden',
-    'gates', 'gauntlet', 'gauzy', 'gazing', 'geese', 'giant',
-    'glances', 'going', 'grapes', 'grass', 'grasses', 'guards',
-    'guided', 'gunshots', 'harbor', 'heart', 'heaven', 'hillside',
-    'holding', 'horse', 'house', 'houses', 'hundred', 'hydrangea',
-    'idling', 'image', 'insane', 'interrogation', 'invisible',
-    'irrevocable', 'itself', 'journey', 'juice', 'karma', 'killed',
-    'knotty', 'knowing', 'knowledge', 'later', 'leave', 'leaving',
-    'letting', 'light', 'lightning', 'lilacs', 'limit', 'little',
-    'lodging', 'longing', 'looks', 'loving', 'making', 'mantle',
-    'marshes', 'memories', 'messengers', 'meteor', 'midnight',
-    'might', 'mirror', 'mirrored', 'missed', 'month', 'moonlight',
-    'mother', 'motorcycle', 'mouth', 'moving', 'myself', 'night',
-    'nightingale', 'nights', 'north', 'nothing', 'nowhere', 'ocean',
-    'octopus', 'opening', 'orchid', 'other', 'paradise', 'parting',
-    'passes', 'passions', 'pattern', 'pealing', 'pears', 'people',
-    'period', 'petal', 'place', 'plain', 'planters', 'playing',
-    'poems', 'poppy', 'press', 'primal', 'primeval', 'purple',
-    'quivered', 'rabbits', 'radiation', 'radio', 'rapids', 'reaches',
-    'reality', 'really', 'recklessly', 'reconciled', 'relax',
-    'remember', 'replies', 'returning', 'right', 'ripple', 'ripples',
-    'rising', 'river', 'riverbank', 'rocky', 'rowing', 'running',
-    'saying', 'seals', 'seeing', 'serpent', 'shadow', 'shall',
-    'shaped', 'shattered', 'shell', 'shelves', 'shift', 'shining',
-    'shore', 'short', 'shower', 'sided', 'silkworm', 'silkworms',
-    'single', 'sleep', 'slept', 'slightest', 'slowly', 'smell',
-    'snail', 'soiled', 'soldiers', 'solitary', 'somehow', 'something',
-    'sometimes', 'sound', 'speak', 'spill', 'spilling', 'spray',
-    'spreads', 'spring', 'squid', 'stable', 'stars', 'station',
-    'steel', 'stirrups', 'stolen', 'stomach', 'stone', 'storm',
-    'straighten', 'strands', 'strange', 'straw', 'streaming',
-    'stripes', 'study', 'submit', 'summer', 'sunlight', 'sunrise',
-    'sunset', 'sutra', 'sweet', 'swimsuit', 'tangled', 'taste',
-    'temple', 'tethered', 'their', 'there', 'these', 'thighs',
-    'thing', 'things', 'think', 'thought', 'thousand', 'throat',
-    'through', 'throughout', 'tiger', 'tight', 'tossing', 'total',
-    'toward', 'trace', 'transferred', 'traps', 'truth', 'turning',
-    'turns', 'twilight', 'unborn', 'under', 'utterly', 'vanished',
-    'village', 'visible', 'waiting', 'wandering', 'warrior',
-    'warriors', 'washed', 'water', 'waves', 'weight', 'where',
-    'which', 'whistling', 'white', 'whitecaps', 'willow', 'wings',
-    'winter', 'wisteria', 'without', 'woman', 'world', 'yanking',
-    'years', 'yesterday', 'yielded', 'young']
-
+def get_default_words():
+    # These are some good default words used in the poem creator above
+    for_poem = [  # emerging tech shit
+        'Agricultural', 'ecological', 'systems', 'meat', 'genetically',
+        'modified', 'precision', 'vertical', 'farming', 'printing', 'contour',
+        'crafting', 'artificial', 'uterus', 'transplant', 'cryonics',
+        'vitrification', 'suspended animation', 'de-extinction',
+        'genetic engineering', 'gene therapy', 'life extension',
+        'engineered negligible senescence',
+        'nanomedicine', 'nanosensors', 'regenerative', 'medicine',
+        'stem-cell', 'tissue engineering', 'robot assisted surgery',
+        'synthetic biology', 'synthetic genomics', 'virus',
+        'whole genome sequencing', 'bionic contact lens',
+        'head-mounted display', 'virtual',
+        'retinal', 'e-textiles', 'molecular', 'electronics', 'thermal',
+        'copper', 'pillar', 'airborne wind turbine', 'artificial',
+        'photosynthesis', 'biofuels', 'solar', 'power', 'fusion', 'fuel cell',
+        'molten salt', 'photovoltaic', 'translation', 'machine vision',
+        'speech recognition', 'fourth-generation', 'optical discs', 'storage',
+        'holographic data', 'millipede', 'optical computing',
+        'quantum computing', 'quantum cryptography', 'RFID', 'software-defined',
+        'three-dimensional', 'integrated', 'circuit', 'artificial muscle',
+        'superconductivity', 'superfluidity', 'metamaterials', 'cloaking',
+        'metal', 'multi-function', 'superalloy', 'synthetic diamond',
+        'weapon', 'laser', 'particle-beam', 'coilgun', 'plasma', 'stealth',
+        'brain computer interface', 'retinal implant',
+        'self reconfiguring modular robot', 'swarm robotics', 'pulse',
+        'solar sail', 'backpack',
+        'helicopter', 'delivery drone', 'detonation', 'engine', 'driverless\
+        car', 'automated', 'vacuum', 'collection', 'cloak', 'immersive',
+        'dilemma',
+        # japanese shit
+        'august', 'black', 'chinese', 'Gaugin', 'heaven', 'illusion',
+        'island', 'Kibune', 'Michinoku', 'milky', 'Mogami', 'mother',
+        'mount', 'mountain', 'Musashi', 'night', 'observe', 'October',
+        'portrait', 'river', 'Roman', 'SUNSHINE', 'should', 'submit',
+        'tangled', 'Tokiwa', 'washing', 'watching', 'world', 'Yoshino',
+        'actual', 'admires', 'after', 'afterlife', 'again', 'against',
+        'alive', 'almost', 'always', 'amidah', 'ancient', 'another',
+        'armor', 'armored', 'arrayed', 'arrows', 'autumn', 'autumns',
+        'awakening', 'bamboo', 'bathe', 'beads', 'become', 'becoming',
+        'begins', 'behind', 'between', 'beyond', 'birth', 'blade',
+        'blind', 'bloom', 'blooming', 'blossoms', 'break', 'breaks',
+        'breeze', 'bridge', 'brings', 'brother', 'brush', 'buried',
+        'burning', 'butterfly', 'calligraphy', 'calling', 'camellia',
+        'cancer', 'candle', 'canyon', 'caress', 'carry', 'ceaseless',
+        'cedars', 'center', 'certain', 'change', 'chanted', 'chases',
+        'cherries', 'cherry', 'child', 'chill', 'chorus', 'chrysanthemum',
+        'chrysanthemums', 'cicada', 'clock', 'closer', 'color', 'combing',
+        'compare', 'completely', 'content', 'continent', 'corona',
+        'could', 'crest', 'crossing', 'curve', 'dancers', 'darkens',
+        'darkness', 'death', 'deepens', 'delusions', 'deserted',
+        'destitute', 'distance', 'dream', 'dreaming', 'dreams', 'drips',
+        'drops', 'drums', 'dying', 'early', 'eclipse', 'egret', 'ended',
+        'entangling', 'escaped', 'evening', 'every', 'exhausted',
+        'faintly', 'falling', 'falls', 'feeling', 'field', 'finished',
+        'fireflies', 'firefly', 'fireworks', 'first', 'flash', 'flesh',
+        'flies', 'float', 'flowers', 'flowing', 'flows', 'follow',
+        'forever', 'forlorn', 'forth', 'fragile', 'frozen', 'garden',
+        'gates', 'gauntlet', 'gauzy', 'gazing', 'geese', 'giant',
+        'glances', 'going', 'grapes', 'grass', 'grasses', 'guards',
+        'guided', 'gunshots', 'harbor', 'heart', 'heaven', 'hillside',
+        'holding', 'horse', 'house', 'houses', 'hundred', 'hydrangea',
+        'idling', 'image', 'insane', 'interrogation', 'invisible',
+        'irrevocable', 'itself', 'journey', 'juice', 'karma', 'killed',
+        'knotty', 'knowing', 'knowledge', 'later', 'leave', 'leaving',
+        'letting', 'light', 'lightning', 'lilacs', 'limit', 'little',
+        'lodging', 'longing', 'looks', 'loving', 'making', 'mantle',
+        'marshes', 'memories', 'messengers', 'meteor', 'midnight',
+        'might', 'mirror', 'mirrored', 'missed', 'month', 'moonlight',
+        'mother', 'motorcycle', 'mouth', 'moving', 'myself', 'night',
+        'nightingale', 'nights', 'north', 'nothing', 'nowhere', 'ocean',
+        'octopus', 'opening', 'orchid', 'other', 'paradise', 'parting',
+        'passes', 'passions', 'pattern', 'pealing', 'pears', 'people',
+        'period', 'petal', 'place', 'plain', 'planters', 'playing',
+        'poems', 'poppy', 'press', 'primal', 'primeval', 'purple',
+        'quivered', 'rabbits', 'radiation', 'radio', 'rapids', 'reaches',
+        'reality', 'really', 'recklessly', 'reconciled', 'relax',
+        'remember', 'replies', 'returning', 'right', 'ripple', 'ripples',
+        'rising', 'river', 'riverbank', 'rocky', 'rowing', 'running',
+        'saying', 'seals', 'seeing', 'serpent', 'shadow', 'shall',
+        'shaped', 'shattered', 'shell', 'shelves', 'shift', 'shining',
+        'shore', 'short', 'shower', 'sided', 'silkworm', 'silkworms',
+        'single', 'sleep', 'slept', 'slightest', 'slowly', 'smell',
+        'snail', 'soiled', 'soldiers', 'solitary', 'somehow', 'something',
+        'sometimes', 'sound', 'speak', 'spill', 'spilling', 'spray',
+        'spreads', 'spring', 'squid', 'stable', 'stars', 'station',
+        'steel', 'stirrups', 'stolen', 'stomach', 'stone', 'storm',
+        'straighten', 'strands', 'strange', 'straw', 'streaming',
+        'stripes', 'study', 'submit', 'summer', 'sunlight', 'sunrise',
+        'sunset', 'sutra', 'sweet', 'swimsuit', 'tangled', 'taste',
+        'temple', 'tethered', 'their', 'there', 'these', 'thighs',
+        'thing', 'things', 'think', 'thought', 'thousand', 'throat',
+        'through', 'throughout', 'tiger', 'tight', 'tossing', 'total',
+        'toward', 'trace', 'transferred', 'traps', 'truth', 'turning',
+        'turns', 'twilight', 'unborn', 'under', 'utterly', 'vanished',
+        'village', 'visible', 'waiting', 'wandering', 'warrior',
+        'warriors', 'washed', 'water', 'waves', 'weight', 'where',
+        'which', 'whistling', 'white', 'whitecaps', 'willow', 'wings',
+        'winter', 'wisteria', 'without', 'woman', 'world', 'yanking',
+        'years', 'yesterday', 'yielded', 'young']
+    return for_poem
 
 if __name__ == '__main__':
     main()
